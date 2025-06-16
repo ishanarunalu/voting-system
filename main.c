3636474c21ab98aab79d2940a615e239ce563cdb
ishanarunalu <ishancampus@gmail.com>
2025-06-16 20:57:17 +0530

    display header implemented

--- main.c
+++ main.c
@@ -46,3 +46,18 @@ int main() {
     mainMenu();
     return 0;
 }
+
+
+void displayHeader() {
+    setColor(11); // Cyan
+    printf("==========================================================\n");
+    printf("=  W   W  EEEEE  L       CCCC   OOO   M         M  EEEEE =\n");
+    printf("=  W   W  E      L      C      O   O  M M     M M  E     =\n");
+    printf("=  W W W  EEEE   L      C      O   O  M  M   M  M  EEEE  =\n");
+    printf("=  WW WW  E      L      C      O   O  M   M M   M  E     =\n");
+    printf("=  W   W  EEEEE  LLLLL   CCCC   OOO   M    M    M  EEEEE =\n");
+    printf("==========================================================\n");
+    printf("=                    TO VOTING SYSTEM                    =\n");
+    printf("==========================================================\n\n");
+    setColor(7); // Reset
+}
