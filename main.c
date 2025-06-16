6032ad338fe1301aeb1c78281cabe8cdfdc7eb41
ishanarunalu <ishancampus@gmail.com>
2025-06-16 20:54:14 +0530

    init c method added

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#define MAX_CANDIDATES 50
+#define MAX_VOTERS 1000
+#define NAME_LEN 50
+#define ID_LEN 20
+
+int main() {
+
+}
